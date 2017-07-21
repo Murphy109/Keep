f702f88ca299e4d931971ba09033ad11ffa91858
Murphy <1104325150@qq.com>
2017-07-21 09:19:19 +0800

    新增首页、发现页

---
+++ public/main.js
@@ -0,0 +1,12 @@
+"use strict"
+require("./js/index.js");
+
+require("./css/public.css");
+
+require("./css/swiper.css");
+
+require("./css/index.css");
+
+require("./js/common.js");
+
+require("./js/swiper.js");
